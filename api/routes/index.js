1266bf7f76ef50273dca53f8aa31fe217df1092a
dimamo5 <diogomoura13@gmail.com>
2017-03-11 19:56:43 +0000

    Routes for baic GET requests

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,20 +1,80 @@
 const express = require('express');
 const infermedica = require('./infermedica');
+const mw = require('./merrian_webster');
 const router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.send('index');
 });
 
-router.get('/refresh_info',function(req,res){
-    Promise.all([infermedica.getConditions(),infermedica.getLabTests(),infermedica.getRiskFactors(),infermedica.getSymptoms()])
-    .then(()=>{
+router.get('/refresh_info', function (req, res) {
+  Promise.all([infermedica.getConditions(), infermedica.getLabTests(), infermedica.getRiskFactors(), infermedica.getSymptoms()])
+    .then(() => {
       res.sendStatus(200);
     })
-    .catch(()=>{
+    .catch(() => {
       res.sendStatus(500);
     })
+})
+
+router.get('/symptoms', function (req, res) {
+  fs.readFile('resources/list_symptoms.json', function (err, data) {
+    if (err) {
+      console.log('Error Reading File with List of Symptoms');
+      res.sendStatus(500);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  })
+})
+
+router.get('/riskfactors', function (req, res) {
+  fs.readFile('resources/list_risk_factors.json', function (err, data) {
+    if (err) {
+      console.log('Error Reading File with List of Risk Factors');
+      res.sendStatus(500);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  })
+})
+
+router.get('/labtests', function (req, res) {
+  fs.readFile('resources/list_lab_tests.json', function (err, data) {
+    if (err) {
+      console.log('Error Reading File with List of Lab Tests');
+      res.sendStatus(500);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  })
+})
+
+router.get('/conditions', function (req, res) {
+  fs.readFile('resources/list_conditions.json', function (err, data) {
+    if (err) {
+      console.log('Error Reading File with List of Conditions', err);
+      res.sendStatus(500);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  })
+})
+
+router.get('/info', function (req, res) {
+  if (req.query.s) {
+    mw.getInfoTerm(req.query.s)
+      .then((data) => {
+        //call function to parse the XML and output only the necessary thing
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log('Error geting the medical information');
+        res.sendStatus(500);
+      })
+  }
 })
 
 module.exports = router;
