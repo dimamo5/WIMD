0885f25868852fff363cfe52f3fb4ecdf14150e9
sergio <sergio.mieic@gmail.com>
2017-04-13 19:02:05 +0100

    Finished and tested with some terms (cancer, parkinson, alzheimer..).

--- api/merriam-webster/parser.js
+++ api/merriam-webster/parser.js
@@ -1,12 +1,62 @@
-const xml2js = require('xml2js');
+const libxmljs = require("libxmljs");
 
-function parse(xmlData){
+function parse(xmlData) {
 
-    let parseString = xml2js.parseString;
+    let xmlDoc = libxmljs.parseXml(xmlData);
 
-    parseString(xmlData, function (err, result) {
-        console.dir(result);
-    });
+    var entries = xmlDoc.get('//entry');
+
+    if (entries == null) {
+        console.log("no results");
+        return null;
+    }
+
+    //console.log(entries.toString());
+
+    try {
+        //remove unnecessary info
+        if(entries.get('.//ca') !== undefined)
+            entries.get('.//ca').remove();
+    }catch(err){
+        console.log(err.toString());
+    }
+
+    let result = {
+        term: entries.get('.//ew').text(),
+        definition: entries.get('.//dt').text()
+    };
+
+    return result;
 }
 
-module.exports = { parse };
+
+/*
+ function parse(xmlData){
+
+ let parser = new xml2js.Parser({explicitArray : false});
+ let parseString = parser.parseString;
+
+ parseString(xmlData, function (err, result) {
+ //console.dir(result.entry_list.entry[0].def.sensb.sens.dt);
+
+ let dataInJson = JSON.parse(JSON.stringify(result.entry_list.entry[0].def.sensb.sens.dt));
+ var definition = "";
+
+ //console.dir(JSON.stringify(result.entry_list.entry[0].def.sensb.sens.dt));
+
+ console.log(JSON.stringify(result.entry_list.entry[0].def.sensb.sens.dt, null, 4));
+
+ for(const prop of Object.keys(dataInJson)) {
+ if(dataInJson.hasOwnProperty(prop))
+ definition += dataInJson[prop];
+ if(array.hasOwnProperty(prop)){
+ definition += array[prop];
+ }
+ }
+
+ // console.log(definition);
+ //console.dir( result.entry_list.entry[0].def.sensb.sens.dt);
+ });
+ }*/
+
+module.exports = {parse};
--- api/routes/info.js
+++ api/routes/info.js
@@ -5,74 +5,72 @@ const mw = require('../request/merrian_webster');
 const parser = require('../merriam-webster/parser');
 
 router.get('/refresh', function (req, res) {
-  Promise.all([infermedica.getConditions(), infermedica.getLabTests(), infermedica.getRiskFactors(), infermedica.getSymptoms()])
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch(() => {
-      res.sendStatus(500);
-    })
+    Promise.all([infermedica.getConditions(), infermedica.getLabTests(), infermedica.getRiskFactors(), infermedica.getSymptoms()])
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch(() => {
+            res.sendStatus(500);
+        })
 })
 
 "request /api/info/term?s=disease"
 router.get('/term', function (req, res) {
-  if (req.query.s) {
-    mw.getInfoTerm(req.query.s)
-      .then((data) => {
-
-          parser.parse(data);  //tratar a informação ver postman
-
-        res.json(data);
-      })
-      .catch((err) => {
-        console.log('Error geting the medical information');
-        res.sendStatus(500);
-      })
-  }
+    if (req.query.s) {
+        mw.getInfoTerm(req.query.s)
+            .then((data) => {
+                let result = parser.parse(data);
+                res.json(result);  //TODO do lado do cliente verificar se res == null (nao foram encontrados resultados)
+            })
+            .catch((err) => {
+                console.log('Error geting the medical information');
+                res.sendStatus(500);
+            })
+    }
 })
 
 router.get('/symptoms', function (req, res) {
-  fs.readFile('resources/list_symptoms.json', function (err, data) {
-    if (err) {
-      console.log('Error Reading File with List of Symptoms');
-      res.sendStatus(500);
-    } else {
-      res.json(JSON.parse(data));
-    }
-  })
+    fs.readFile('resources/list_symptoms.json', function (err, data) {
+        if (err) {
+            console.log('Error Reading File with List of Symptoms');
+            res.sendStatus(500);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    })
 });
 
-router.get('/riskfactors',function(req,res){
-  fs.readFile('resources/list_risk_factors.json', function (err, data) {
-    if (err) {
-      console.log('Error Reading File with List of Risk Factors');
-      res.sendStatus(500);
-    } else {
-      res.json(JSON.parse(data));
-    }
-  })
+router.get('/riskfactors', function (req, res) {
+    fs.readFile('resources/list_risk_factors.json', function (err, data) {
+        if (err) {
+            console.log('Error Reading File with List of Risk Factors');
+            res.sendStatus(500);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    })
 });
 
-router.get('/labtests',function(req,res){
-  fs.readFile('resources/list_lab_tests.json', function (err, data) {
-    if (err) {
-      console.log('Error Reading File with List of Lab Tests');
-      res.sendStatus(500);
-    } else {
-      res.json(JSON.parse(data));
-    }
-  })
+router.get('/labtests', function (req, res) {
+    fs.readFile('resources/list_lab_tests.json', function (err, data) {
+        if (err) {
+            console.log('Error Reading File with List of Lab Tests');
+            res.sendStatus(500);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    })
 });
 
-router.get('/conditions',function(req,res){
- fs.readFile('resources/list_conditions.json', function (err, data) {
-    if (err) {
-      console.log('Error Reading File with List of Conditions', err);
-      res.sendStatus(500);
-    } else {
-      res.json(JSON.parse(data));
-    }
-  })
+router.get('/conditions', function (req, res) {
+    fs.readFile('resources/list_conditions.json', function (err, data) {
+        if (err) {
+            console.log('Error Reading File with List of Conditions', err);
+            res.sendStatus(500);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    })
 });
 
 
