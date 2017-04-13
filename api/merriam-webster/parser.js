2d609fb15134d8fd567e9723817b65b8442bc813
sergio <sergio.mieic@gmail.com>
2017-04-13 19:05:01 +0100

    Added documentation

--- api/merriam-webster/parser.js
+++ api/merriam-webster/parser.js
@@ -1,5 +1,10 @@
 const libxmljs = require("libxmljs");
 
+/**
+ * Parses xml data related to dictionary term results and returns the util info in JSON format
+ * @param xmlData xml info from Merriam Webter
+ * @returns result in the following format {term: "", definition: ""}
+ */
 function parse(xmlData) {
 
     let xmlDoc = libxmljs.parseXml(xmlData);
@@ -28,35 +33,4 @@ function parse(xmlData) {
 
     return result;
 }
-
-
-/*
- function parse(xmlData){
-
- let parser = new xml2js.Parser({explicitArray : false});
- let parseString = parser.parseString;
-
- parseString(xmlData, function (err, result) {
- //console.dir(result.entry_list.entry[0].def.sensb.sens.dt);
-
- let dataInJson = JSON.parse(JSON.stringify(result.entry_list.entry[0].def.sensb.sens.dt));
- var definition = "";
-
- //console.dir(JSON.stringify(result.entry_list.entry[0].def.sensb.sens.dt));
-
- console.log(JSON.stringify(result.entry_list.entry[0].def.sensb.sens.dt, null, 4));
-
- for(const prop of Object.keys(dataInJson)) {
- if(dataInJson.hasOwnProperty(prop))
- definition += dataInJson[prop];
- if(array.hasOwnProperty(prop)){
- definition += array[prop];
- }
- }
-
- // console.log(definition);
- //console.dir( result.entry_list.entry[0].def.sensb.sens.dt);
- });
- }*/
-
 module.exports = {parse};
